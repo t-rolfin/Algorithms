4e125bd5d84f51ec8aea1fbc68ee2c0dc4276a94
t-rolfin <rolfin.dc@gmail.com>
2021-04-14 16:12:27 +0300

    Added insertion sort.

---
+++ sorting/Insertion.cs
@@ -0,0 +1,41 @@
+﻿using sorting.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting
+{
+    public class Insertion : ISorting
+    {
+        private int[] array;
+
+        public Insertion(int[] array)
+        {
+            this.array = array;
+        }
+
+        public int[] Sort()
+        {
+            int j;
+            int temp;
+            int n = array.Length;
+
+            for (int i = 1; i < n; i++)
+            {
+                temp = array[i];
+                j = i - 1;
+
+                while ((temp < array[j]) && (j >= 0))
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = temp;
+            }
+
+            return this.array;
+        }
+    }
+}
